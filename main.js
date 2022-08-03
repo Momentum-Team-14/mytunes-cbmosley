189178009471ca84bb7161f76198cc26925d16ad
cbmosley <cbmosley@ncsu.edu>
2022-08-03 15:04:15 -0400

    audio added to each result

--- main.js
+++ main.js
@@ -1,64 +1,88 @@
+//creating conatiners for each section of the page
+const audioContainer = document.querySelector('#audiocontianer')
+
 const searchContainer = document.querySelector('#searchcontainer')
-const mediaContainer = document.querySelector('#container')
+
+const mediaContainer = document.querySelector('#mediacontainer')
 
 let resultsDiv = document.querySelector('#results')
 console.log('results div', resultsDiv)
 
-let searchUrl = 'https://itunes.apple.com/search?term=masego'
+let searchBaseUrl = 'https://itunes.apple.com/search?term='
 
 //search bar div and type bar and search button
-let searchBox = document.createElement('div');
-searchBox.classList.add('searchbar')
-
-let searchBar = document.createElement('input');
-searchBar.classList.add('searchbar');
-
-let searchButton = document.createElement('button');
-searchButton.innerText = 'Search'
-
-//append
-searchBox.appendChild(searchBar);
-searchBox.appendChild(searchButton);
-
-searchContainer.appendChild(searchBox);
-
-
-
-fetch(searchUrl, {
-    method: 'GET',
-    headrs: {'Content-Type': 'application/json'}
+let searchForm = document.querySelector('#search-form')
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log('Submitted!')
+    let searchBox = document.querySelector("#search-box")
+    let searchUrl = `${searchBaseUrl}${searchBox.value}`
+    console.log(searchBox.value)
+    getSearchResults(searchUrl)
 })
-//response is whatever the fetch returns
-.then(function (response){
-    return response.json()
-})
-//data is whatever the above code returns, in this case response.json()
-.then(function (data){
-    let songs = data.results
-    console.log(songs)
-    for (let song of songs){
-    //add div and class
-        let mediaDiv = document.createElement('div');
-        mediaDiv.classList.add('media')
+function getSearchResults(url){
+    fetch(url, {
+        method: 'GET',
+        headrs: {'Content-Type': 'application/json'}
+    })
+    //response is whatever the fetch returns
+    .then(function (response){
+        return response.json()
+    })
+    //data is whatever the above code returns, in this case response.json()
+    .then(function (data){
+        let songs = data.results
+        console.log(songs)
+        for (let song of songs){
+        //add div and class
+            let mediaDiv = document.createElement('div');
+            mediaDiv.classList.add('media')
+    
+            let albumCoverDiv = document.createElement('img');
+            albumCoverDiv.classList.add('albumcover');
+            albumCoverDiv.src = `${song.artworkUrl100}`
+            let songTitleDiv = document.createElement('div');
+            songTitleDiv.classList.add('songtitle');
+            songTitleDiv.innerText = `${song.trackName}`
+            let artistNameDiv = document.createElement('div');
+            artistNameDiv.classList.add('artist');
+            artistNameDiv.innerText = `${song.artistName}`
+            
+            let audioTag = document.createElement("audio");
+            audioTag.classList.add('audio');
+            audioTag.src = song.previewUrl;
+            audioTag.controls = true;
 
-        let albumCoverDiv = document.createElement('img');
-        albumCoverDiv.classList.add('albumcover');
-        albumCoverDiv.src = `${song.artworkUrl100}`
-        let songTitleDiv = document.createElement('div');
-        songTitleDiv.classList.add('songtitle');
-        songTitleDiv.innerText = `${song.trackName}`
-        let artistNameDiv = document.createElement('div');
-        artistNameDiv.classList.add('artist');
-        artistNameDiv.innerText = `${song.artistName}`
 
-//append
-        mediaDiv.appendChild(albumCoverDiv);
-        mediaDiv.appendChild(songTitleDiv);
-        mediaDiv.appendChild(artistNameDiv);
-        mediaContainer.appendChild(mediaDiv);
-    }
+    //append
+            mediaDiv.appendChild(albumCoverDiv);
+            mediaDiv.appendChild(songTitleDiv);
+            mediaDiv.appendChild(artistNameDiv);
+            mediaDiv.appendChild(audioTag);
+            mediaContainer.appendChild(mediaDiv);
+        }
+    
+    })
+   
 
-})
+}
+// let searchBox = document.createElement('div');
+// searchBox.classList.add('searchbar')
+
+// let searchBar = document.createElement('input');
+// searchBar.classList.add('searchbar');
+
+// let searchButton = document.createElement('button');
+// searchButton.innerText = 'Search'
+
+// //append
+// searchBox.appendChild(searchBar);
+// searchBox.appendChild(searchButton);
+
+// searchContainer.appendChild(searchBox);
+
+
+
 
 
 
