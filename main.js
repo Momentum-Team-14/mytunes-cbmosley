60a6111c3024dd1eafabb92d6366fbfda3c073b3
cbmosley <cbmosley@ncsu.edu>
2022-08-04 11:25:58 -0400

    Added clear Container function and called the clear function before the media Container is called every time.

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ const audioContainer = document.querySelector('#audiocontianer')
 const searchContainer = document.querySelector('#searchcontainer')
 
 const mediaContainer = document.querySelector('#mediacontainer')
-
+//mediaContainer.innerHTML = "Search Results: "
 let resultsDiv = document.querySelector('#results')
 console.log('results div', resultsDiv)
 
+
+//the url for the search form
 let searchBaseUrl = 'https://itunes.apple.com/search?term='
 
 //search bar div and type bar and search button
 let searchForm = document.querySelector('#search-form')
+
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault()
     console.log('Submitted!')
@@ -23,7 +26,7 @@ searchForm.addEventListener('submit', (event) => {
 function getSearchResults(url){
     fetch(url, {
         method: 'GET',
-        headrs: {'Content-Type': 'application/json'}
+        headers: {'Content-Type': 'application/json'}
     })
     //response is whatever the fetch returns
     .then(function (response){
@@ -32,12 +35,15 @@ function getSearchResults(url){
     //data is whatever the above code returns, in this case response.json()
     .then(function (data){
         let songs = data.results
+            // Need to clear old results out of the mediaContainer
+            //called the clearFunction 
+            clearContainer(mediaContainer);
         console.log(songs)
         for (let song of songs){
         //add div and class
             let mediaDiv = document.createElement('div');
             mediaDiv.classList.add('media')
-    
+            //mediaDiv.innerText = "Search Results:"
             let albumCoverDiv = document.createElement('img');
             albumCoverDiv.classList.add('albumcover');
             albumCoverDiv.src = `${song.artworkUrl100}`
@@ -60,11 +66,18 @@ function getSearchResults(url){
             mediaDiv.appendChild(artistNameDiv);
             mediaDiv.appendChild(audioTag);
             mediaContainer.appendChild(mediaDiv);
+
         }
     
     })
    
 
+}
+//function to clear containers
+function clearContainer(container){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
 // let searchBox = document.createElement('div');
 // searchBox.classList.add('searchbar')
